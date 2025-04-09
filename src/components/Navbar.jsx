e7332af9343d9813253d9ef8e2db6b9285a62bf3
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-04-09 03:49:46 +0000

    cambio de nombre y creacion de AddContact.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
 				</Link>
 				<div className="ml-auto">
 					<Link to="/addcontact">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-Success">Add new contact</button>
 					</Link>
 				</div>
 			</div>
