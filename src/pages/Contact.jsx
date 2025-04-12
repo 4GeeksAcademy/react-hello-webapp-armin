192e8db423b922eab995eea1dd5df3fee882ecff
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-04-12 21:07:57 +0000

    cambios

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,53 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { ContactCard } from "../components/ContactCard.jsx";
+import { useContacts } from "../hooks/userContacts.jsx";
 
 export const Contact = () => {
+  
+  const {
+    contacts,
+    deleteAllContacts,
+    deleteContactById
+  } = useContacts()
 
-  const {store, dispatch} =useGlobalReducer()
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 mb-3 d-flex justify-content-center ">
+          <div className="w-75">
+            <Link to="/AddContact">
+              <button className="btn btn-success w-100">Add new contact</button>
+            </Link>
+          </div>
+        </div>
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+        <div className="col-12 d-flex justify-content-center">
+          <button
+            type="button"
+            className="btn btn-danger mb-3 w-75 "
+            onClick={deleteAllContacts}
+          >
+            Delete all users
+          </button>
+        </div>
+
+        <div className="col-9 mx-auto">
+          <div className="card">
+            <ul className="list-group list-group-flush">
+              {contacts.map((contact) => {
+                return (
+                  <ContactCard
+                    key={contact.id}
+                    contact={contact}
+                    onDelete={deleteContactById}
+                  />
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
