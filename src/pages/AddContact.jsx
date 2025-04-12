f8575b18ccf12e98b06dda90db1d08927ae88dae
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-04-12 21:05:23 +0000

    cambios

--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -1,67 +1,55 @@
 // Import necessary components from react-router-dom and other parts of the application.
-import { Link } from "react-router-dom";
-import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
+import {useNavigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx"; 
+import { createContac, getContacts } from "../services/fetchApi.js";
+import React, { useState } from "react";
+import { Form } from "../components/Form.jsx";
 
 export const AddContact = () => {
-  // Access the global state and dispatch function using the useGlobalReducer hook.
-  const { store, dispatch } = useGlobalReducer()
+  const { dispatch } = useGlobalReducer();
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
+
+  const handleChange = (field, value) => {
+    setFormData({ ...formData, [field]: value });
+  };
+
+  const handleSubmit = async () => {
+    if (
+      !formData.name ||
+      !formData.email ||
+      !formData.phone ||
+      !formData.address
+    ) {
+      setError("Please fill out all fields.");
+      return;
+    }
+
+    const created = await createContac(formData);
+    if (created) {
+      const contacts = await getContacts();
+      dispatch({ type: "update_contacts", payload: { contacts } });
+
+      navigate("/");
+      setError(null);
+      alert("Successfully created user");
+    }
+  };
 
   return (
-    <div className="container">
-      <div className="d-flex justify-content-center">
-        <h1>Add a new Contact</h1>
-
-      </div>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="fullName" className="form-label">Full Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="fullName"
-            aria-describedby="emailHelp"
-            placeholder="Full Name"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="phone" className="form-label">Phone</label>
-          <input
-            type="tel"
-            className="form-control"
-            id="phone"
-            aria-describedby="emailHelp"
-            placeholder="Enter Phone"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="address" className="form-label">Address</label>
-          <input
-            type="text"
-            className="form-control"
-            id="address"
-            placeholder="Enter address"
-          />
-        </div>
-        
-        <Link to="/">
-          <button className="btn btn-primary mb-3">Save</button>
-        </Link>
-
-      </form>
-
-      <Link className="form-check-label" htmlFor="exampleCheck1">
-        or get back to contacts
-      </Link>
-    </div>
+    <Form
+      title="Create a new Contact"
+      formData={formData}
+      onChange={handleChange}
+      onSubmit={handleSubmit}
+      error={error}
+      buttonText="Save"
+    />
   );
 };
