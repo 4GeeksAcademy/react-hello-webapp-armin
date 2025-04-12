0b1d545d5330220d4d35519b68125f0db754654c
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-04-12 22:14:53 +0000

    fedback de los maestros a haber que pedos

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,53 +1,37 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ContactCard } from "../components/ContactCard.jsx";
-import { useContacts } from "../hooks/userContacts.jsx";
+import { userContacts } from "../hooks/userContacts.jsx";
 
 export const Contact = () => {
-  
-  const {
-    contacts,
-    deleteAllContacts,
-    deleteContactById
-  } = useContacts()
 
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-12 mb-3 d-flex justify-content-center ">
-          <div className="w-75">
-            <Link to="/AddContact">
-              <button className="btn btn-success w-100">Add new contact</button>
-            </Link>
-          </div>
-        </div>
+	const {
+		contacts,
+		deleteAllContacts,
+		deleteContactById
+	} = userContacts()
 
-        <div className="col-12 d-flex justify-content-center">
-          <button
-            type="button"
-            className="btn btn-danger mb-3 w-75 "
-            onClick={deleteAllContacts}
-          >
-            Delete all users
-          </button>
-        </div>
+	return (
+		<div className="container mt-5">
+			<div className="row">
+				
 
-        <div className="col-9 mx-auto">
-          <div className="card">
-            <ul className="list-group list-group-flush">
-              {contacts.map((contact) => {
-                return (
-                  <ContactCard
-                    key={contact.id}
-                    contact={contact}
-                    onDelete={deleteContactById}
-                  />
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="col-9 mx-auto">
+					<div className="card">
+						<ul className="list-group list-group-flush">
+							{contacts.map((contact) => {
+								return (
+									<ContactCard
+										key={contact.id}
+										contact={contact}
+										onDelete={deleteContactById}
+									/>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/routes.jsx
+++ src/routes.jsx
@@ -24,7 +24,7 @@ export const router = createBrowserRouter(
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Contact />} />
         <Route path="/AddContact" element={ <AddContact />} />  {/* Dynamic route for single items */}
-        <Route path="/Addcontact/:id" element={<EdContact />} />
+        <Route path="/EdContact/:id" element={<EdContact />} />
       </Route>
     )
 );
